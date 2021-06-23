849f03f832c2540cdc113738e59bea9351575bc3
kunal2008-school <80512329+kunal2008-school@users.noreply.github.com>
2021-06-23 14:57:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,72 @@
+
+var block1,block2,block3,block4;
+var ball, edges;
+
+
+function preload(){
+   
+ music = loadSound("music.mp3");
+}
+
+function setup(){
+    canvas = createCanvas(800,600);
+
+   
+
+    block1 = createSprite(10,590,360,30);
+    block1.shapeColor = "blue";
+
+    block2 = createSprite(280,590,210,30);
+    block2.shapeColor = "yellow";
+
+    block3 = createSprite(493,590,210,30)
+    block3.shapeColor = "red";
+
+    block4 = createSprite(780,590,360,30)
+    block4.shapeColor = "green";
+
+    ball = createSprite(250,100, 40,40);
+    ball.shapeColour = rgb (225,225,225);
+    ball.velocityY = 7
+    ball.velocityX = 3
+
+   
+
+}
+
+function draw() {
+    background(rgb(169,169,169));
+   
+
+   
+    edges=createEdgeSprites();
+    ball.bounceOff(edges);
+
+    
+
+    if(block1.isTouching(ball) && ball.bounceOff(block1)){     
+     ball.shapeColor = "blue";  
+     music.play();
+    }
+
+    if(block2.isTouching(ball) && ball.bounceOff(block2)){
+        ball.shapeColor = "yellow";     
+        ball.velocityX = 0
+        ball.velocityY = 0
+       music.stop()
+     }
+  
+    if(block3.isTouching(ball) && ball.bounceOff(block3)){
+       ball.shapeColor = "red"; 
+       music.play();
+    }
+    
+    if(block4.isTouching(ball) && ball.bounceOff(block4)){
+         ball.shapeColor = "green"; 
+         music.play();
+        }    
+    
+   
+   
+    drawSprites()
+}
